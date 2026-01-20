606088be5659ffc2f5b9b0f49d723d0abd3506fc
Abby Danger <abbyadanger@gmail.com>
2026-01-19 22:46:21 -0600

    Fix? Maybe?

--- src/app/film/film.component.ts
+++ src/app/film/film.component.ts
@@ -1,5 +1,5 @@
 import { CommonModule } from '@angular/common';
-import { Component, OnInit, signal, HostListener } from '@angular/core';
+import { Component, OnInit, signal, HostListener, ChangeDetectorRef } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { HttpClient } from '@angular/common/http';
 
@@ -17,19 +17,23 @@ export class FilmComponent implements OnInit {
   currentIndex: number = 0;
   filmURLPath = '/docs/pics/film/';
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient, private cdr: ChangeDetectorRef) { }
 
   ngOnInit(): void {
     // Retrieve list of filenames for pictures
     this.http.get<string[]>('./film-list.json').subscribe({
       next: (filenames) => {
+        console.log('Raw filenames received:', filenames);
         this.filenames = this.randomizeOrder(filenames);
         console.log(`Loaded ${this.filenames.length} film images`);
+        console.log('Filenames array:', this.filenames);
+        this.cdr.detectChanges(); // Force change detection
       },
       error: (error) => {
         console.error('Error loading film list:', error);
         console.error('Tried to load: ./film-list.json');
         this.filenames = [];
+        this.cdr.detectChanges();
       }
     });
   }
