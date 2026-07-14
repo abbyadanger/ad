58bb18b8ace0147ea941e0d8d20524da568e8c11
Abby Danger <abbyadanger@gmail.com>
2026-07-14 10:00:55 -0500

    Footer component

--- src/app/app.ts
+++ src/app/app.ts
@@ -9,11 +9,12 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
 
 @Component({
   selector: 'app-root',
-  imports: [CommonModule, RouterModule, HeaderComponent],
-  template: '<app-header></app-header><router-outlet></router-outlet>',
+  imports: [CommonModule, RouterModule, HeaderComponent, FooterComponent],
+  template: '<app-header></app-header><router-outlet></router-outlet><app-footer></app-footer>',
 })
 export class App {
 }
