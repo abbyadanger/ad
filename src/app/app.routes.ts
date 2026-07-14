75c62018358c3731c656a02986758181e224aa04
Abby Danger <abbyadanger@gmail.com>
2026-07-14 12:37:22 -0500

    Final Touches

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,12 +3,12 @@
 */
 
 import { Routes } from '@angular/router';
-import { AboutComponent } from './about/about.component';
+import { MeComponent } from './me/me.component';
 import { WorkComponent } from './work/work.component';
 import { FilmComponent } from './film/film.component';
 
 export const routes: Routes = [
-  { path: '', component: AboutComponent },
+  { path: '', component: MeComponent },
   { path: 'work', component: WorkComponent },
   { path: 'film', component: FilmComponent },
   { path: '**', redirectTo: '', pathMatch: 'full' }
